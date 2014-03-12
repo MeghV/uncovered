6dd2428b45c77fddd9baa1ad6b482285d4344527
Megh Vakharia <meghhv@gmail.com>
2014-03-12 11:40:26 -0700

    changed test.php to replace to absolute links

--- test.php
+++ test.php
@@ -1,5 +1,9 @@
 <?php 
 
 $uber = file_get_contents('https://www.uber.com/');
+$domain = "https://www.uber.com/";
+$uber = preg_replace("/(href|src)\=\"([^(http)])(\/)?/", "$1=\"$domain$2", $uber);
 
 echo $uber;
+
+?>
