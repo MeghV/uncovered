de8f0c888ff2177d440c9084e18b33a60107c924
Megh Vakharia <meghhv@gmail.com>
2014-03-12 11:41:29 -0700

    removed slash

--- test.php
+++ test.php
@@ -1,7 +1,7 @@
 <?php 
 
 $uber = file_get_contents('https://www.uber.com/');
-$domain = "https://www.uber.com/";
+$domain = "https://www.uber.com";
 $uber = preg_replace("/(href|src)\=\"([^(http)])(\/)?/", "$1=\"$domain$2", $uber);
 
 echo $uber;
